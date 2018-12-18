e9db9b245054dbe7551d1ef5b750297acca40dd4
Aimee <baines@admins-iMac-4.local>
2018-12-18 17:23:35 +0000

    added fetch to API - need fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
+import { createClient } from 'contentful';
 
 export default class Home extends Component {
+  state = {
+    posts: []
+  };
+
+  componentWillMount(){
+    const client = createClient({
+      space: process.env.REACT_APP_SPACE_ID,
+      accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    });
+
+    client
+      .getEntries({})
+      .then(res => {
+        this.setState({
+          posts: res.items
+        });
+      })
+      .catch(console.error);
+  }
+
+
   render() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-    )
+    if(!this.state.posts.length) return <p>No posts found.</p>;
+    return this.state.posts.map(post => {
+        console.log(post);
+        return <p>Post</p>;
+    });
   }
 }
